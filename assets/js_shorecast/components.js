e3d43724d9940cabc45b5c2784acc60e4b53ca1b
Antoine Jarry <antoinejarry.pro@gmail.com>
2026-04-28 08:54:49 +0200

    title page update

--- assets/js_shorecast/components.js
+++ assets/js_shorecast/components.js
@@ -14,22 +14,3 @@ document.addEventListener("DOMContentLoaded", () => {
   loadComponent("#footer-placeholder", "/components/footer.html");
   loadComponent("#home-placeholder","/pages/home.html");
 });
-
-
-
-// document.addEventListener("click", function(e) {
-//   // Vérifie si on clique sur le lien Contact
-//   const link = e.target.closest('a[href="#footer"]');
-
-//   if (link) {
-//     e.preventDefault(); // bloque le comportement normal
-
-//     const footer = document.getElementById("footer");
-
-//     if (footer) {
-//       footer.scrollIntoView({
-//         behavior: "smooth"
-//       });
-//     }
-//   }
-// });
